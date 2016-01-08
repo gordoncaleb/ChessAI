a27fcec82c3cd4b0890d47acec623c63abc9b17a
Caleb Walker <gordoncaleb@gmail.com>
2016-01-08 23:40:00 +0000

    added engine mount tests

--- core/src/main/java/com/gordoncaleb/chess/engine/EngineMount.java
+++ core/src/main/java/com/gordoncaleb/chess/engine/EngineMount.java
@@ -2,44 +2,50 @@ package com.gordoncaleb.chess.engine;
 
 import com.gordoncaleb.chess.board.Board;
 import com.gordoncaleb.chess.board.Move;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 public class EngineMount {
-    private final ABWithHashEngine engine;
+    private static final Logger LOGGER = LoggerFactory.getLogger(EngineMount.class);
+
+    private final Engine engine;
     private final ExecutorService executorService;
 
-    public EngineMount(ABWithHashEngine engine) {
+    public EngineMount(Engine engine) {
         this.engine = engine;
         this.executorService = Executors.newSingleThreadExecutor();
     }
 
-    public EngineMount(ABWithHashEngine engine, ExecutorService executorService) {
+    public EngineMount(Engine engine, ExecutorService executorService) {
         this.engine = engine;
         this.executorService = executorService;
     }
 
-    public Future<Move> search(final Board board, final long timeTarget) {
+    public Future<List<Move>> search(final Board board, final long timeTarget) {
 
         final long endTime = now() + timeTarget;
         return executorService.submit(() -> {
 
             final List<List<Move>> pvs = new ArrayList<>();
-            final List<Long> times = Arrays.asList(now());
+            final List<Long> times = new ArrayList<>();
+            times.add(now());
 
             int depth = 1;
             while (hasEnoughTime(times, endTime)) {
-                MovePath movePath = engine.iterativeSearch(board, depth);
+                MovePath movePath = engine.search(board, depth);
                 times.add(now());
 
                 pvs.add(movePath.asList());
                 depth++;
             }
+
+            LOGGER.info("times: {}", times);
 
             return searchResult(pvs);
         });
@@ -49,7 +55,7 @@ public class EngineMount {
         return System.currentTimeMillis();
     }
 
-    private boolean hasEnoughTime(List<Long> times, long endTime) {
+    private static boolean hasEnoughTime(List<Long> times, long endTime) {
         if (times.size() > 3) {
             final long timeMinus1 = times.get(times.size() - 1);
             final long timeMinus2 = times.get(times.size() - 2);
@@ -58,7 +64,11 @@ public class EngineMount {
             final double d1 = timeMinus1 - timeMinus2;
             final double d2 = timeMinus2 - timeMinus3;
 
-            final long estimatedTime = timeMinus1 + (long) (d1 * d1 / d2);
+            final double d3 = (d1 * d1 / d2);
+
+            final long estimatedTime = timeMinus1 + (long) (d3);
+
+            LOGGER.info("Delta 1 = {}, Delta 2 = {}, Predicted Next = {}", d1, d2, d3);
 
             return estimatedTime <= endTime;
         }
@@ -66,7 +76,7 @@ public class EngineMount {
         return true;
     }
 
-    private static Move searchResult(List<List<Move>> pvs) {
-        return pvs.get(pvs.size() - 1).get(0);
+    private static List<Move> searchResult(List<List<Move>> pvs) {
+        return pvs.get(pvs.size() - 1);
     }
 }
--- core/src/test/java/com.gordoncaleb.chess/unit/engine/EngineMountTest.java
+++ core/src/test/java/com.gordoncaleb.chess/unit/engine/EngineMountTest.java
@@ -1,19 +1,34 @@
 package com.gordoncaleb.chess.unit.engine;
 
+import com.gordoncaleb.chess.board.Board;
+import com.gordoncaleb.chess.board.Move;
 import com.gordoncaleb.chess.board.MoveContainerFactory;
+import com.gordoncaleb.chess.board.serdes.PGNParser;
 import com.gordoncaleb.chess.engine.ABWithHashEngine;
 import com.gordoncaleb.chess.engine.EngineMount;
 import com.gordoncaleb.chess.engine.score.StaticScorer;
+import com.gordoncaleb.chess.util.Perft;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class EngineMountTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(EngineMountTest.class);
 
     @Test
-    public void testSearchTime(){
+    public void testSearchTime() throws ExecutionException, InterruptedException {
         ABWithHashEngine engine = new ABWithHashEngine(new StaticScorer(),
                 MoveContainerFactory.buildSortableMoveContainers(30));
 
         EngineMount engineMount = new EngineMount(engine);
 
+        Board board = new Perft().kiwiPetePosition();
+
+        List<Move> moveList = engineMount.search(board, 3000).get();
+
+        LOGGER.info(PGNParser.toAlgebraicNotation(moveList, board));
     }
 }
